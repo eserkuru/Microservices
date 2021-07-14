c150b20ff0f83bb1d4962e0a583afcd6a538bcb4
Eser KURU <eser.kuru@infoline-tr.com>
2021-07-14 11:55:19 +0300

    AutoMapper was implemented and mapped dtos.

---
+++ Services/Catalog/Course.Services.Catalog/Mappings/GeneralMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Course.Services.Catalog.Dtos.Category;
+using Course.Services.Catalog.Dtos.Course;
+using Course.Services.Catalog.Dtos.Feature;
+using Course.Services.Catalog.Models;
+
+namespace Course.Services.Catalog.Mappings
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+            CreateMap<Category, CategoryUpdateDto>().ReverseMap();
+
+            CreateMap<Models.Course, CourseDto>().ReverseMap();
+            CreateMap<Models.Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Models.Course, CourseUpdateDto>().ReverseMap();
+
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+        }
+    }
+}
